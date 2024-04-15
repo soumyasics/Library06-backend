c100b17d79c4f3cbf4f6144506661388715379eb
sheik2313 <sheikshahul6@gmail.com>
2024-04-15 12:00:34 +0530

    feedbackbackend

--- router.js
+++ router.js
@@ -2,10 +2,15 @@ var express=require("express")
 var router=express.Router()
 
 var registercontroler=require('./controler/registercontroler')
+var feedbackcontroler= require('./controler/feedbackcontroler')
 
 router.post('/registerdata',registercontroler.studentregisterdata)
 router.post('/find',registercontroler.viewOne)
 router.get('/profileview/:id',registercontroler.findstudentprofile)
 router.get('/findprofileedit/:id',registercontroler.findeditprofile)
 router.post('/editprofile/:id',registercontroler.findprofileupdate)
+router.get('/welcomename/:id',registercontroler.findstudentname)
+
+
+router.post('/studentfeedback',feedbackcontroler.studentfeedbackdata)
 module.exports=router
